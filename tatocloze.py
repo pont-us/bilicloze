3e8dcfef3eb1c1d1f0d71fc7208cad9f208b4aa0
Pontus Lurcock <pont@talvi.net>
2019-10-02 09:57:17 +0200

    Better cloze generation, etc.
     - Cloze generation is now more reliable: words which are substrings of
       other words don't get replaced with the cloze marker.
    
     - Use sentences used in the order they're read from the file, so output
       is deterministic.
    
     - Set limits on maximum numbers of sentences per word, translations
       per sentence, and characters per output string.

--- tatocloze.py
+++ tatocloze.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
+from typing import List
+import re
 import wordfreq
 import argparse
 
 def main():
+    make_clozes(100, 10, 5, 80)
+
+
+def make_clozes(nwords, max_sentences_per_word,
+                max_translations_per_sentence,
+                max_characters):
     parser = argparse.ArgumentParser()
     parser.add_argument("sentence_file", type=str)
     args = parser.parse_args()
 
-    sentence_map = {}
-    # word_map = {}
+    sentence_map = OrderedDict()
+    word_map = {}
     
     with open(args.sentence_file) as fh:
         while True:
@@ -20,15 +29,68 @@ def main():
             if sentence_l2 not in sentence_map:
                 sentence_map[sentence_l2] = []
             sentence_map[sentence_l2].append(sentence_l1)
+            words = sentence_to_words(sentence_l2)
+            for word in words:
+                if word not in word_map:
+                    word_map[word] = []
+                word_map[word].append(sentence_l2)
 
-    top_n_words = wordfreq.top_n_list("fi", 100)
+    top_n_words = wordfreq.top_n_list("fi", nwords)
 
     for word in top_n_words:
-        for sentence_l2 in sentence_map:
-            if word in sentence_l2:
-                cloze = sentence_l2.replace(word, "####")
-                print(cloze, sentence_map[sentence_l2][0], word)
-                break
+        if word in word_map:
+            sentences_l2 = word_map[word][:max_sentences_per_word]
+            for sentence_l2 in sentences_l2:
+                translation_list = \
+                    sentence_map[sentence_l2][:max_translations_per_sentence]
+                cloze = clozify(sentence_l2, word, "____")
+                translations = " / ".join(translation_list)
+                if len(cloze) <= max_characters and \
+                   len(translations) <= max_characters:
+                    print(cloze, translations, word)
 
+
+def clozify(sentence, word, cloze_marker):
+    """Make a cloze out of a sentence.
+
+    >>> clozify("I battered the bat.", "bat", "####")
+    'I battered the ####.'
+    >>> clozify("A fat pig is a happy pig.", "pig", "??")
+    'A fat ?? is a happy ??.'
+    >>> clozify("This isn't too hard.", "isn't", "___")
+    'This ___ too hard.'
+    >>> clozify("It wasn't, alas, correct.", "wasn't", "___")
+    'It ___, alas, correct.'
+    >>> clozify("First things first.", "first", "---")
+    '--- things ---.'
+    """
+
+    return re.subn(r"(^\W*| \W*)" + word + r"(\W* |\W*$)",
+                   r"\1" + cloze_marker + r"\2",
+                   sentence,
+                   flags=re.IGNORECASE)[0]
+
+
+def sentence_to_words(sentence: str) -> List[str]:
+    """Put a string in lower case and remove punctuation between words.
+
+    >>> sentence_to_words("Today, they aren't up-to-date any more.")
+    ['today', 'they', "aren't", 'up-to-date', 'any', 'more']
+    """
+    return list(map(clean_word, sentence.split(" ")))
+
+
+def clean_word(word: str) -> str:
+    """Put a string in lower case and remove surrounding punctuation.
+
+    >>> clean_word("Won't!")
+    "won't"
+    >>> clean_word("Full-time,")
+    'full-time'
+    """
+    return re.sub(r"^[,.'\"()!]+", "",
+                  re.sub(r"[,.'\"()!]+$", "", word.lower()))
+
+            
 if __name__ == "__main__":
     main()
